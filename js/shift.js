4855299e61e3d8185a4e900c66fe9bfb9f26f7d6
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-16 16:56:35 -0500

    fix: remove dark radial vignette and adjust font weight

--- js/shift.js
+++ js/shift.js
@@ -13,7 +13,7 @@ const rangeHue = 60;
 const xOff = 0.0015;
 const yOff = 0.0015;
 const zOff = 0.0015;
-const backgroundColor = 'hsla(0,0%,5%,1)';
+const backgroundColor = 'hsla(0,0%,5%,0)';
 
 let container;
 let canvas;
